852045f96da389c1ddba62997f18e17fdf5d29e1
saif <saifahmed606012@gmail.com>
2023-09-26 11:13:40 +0300

    edits v2 commit

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -1,106 +1,39 @@
-#include "flutter_window.h"
-#include <locale>
-#include <codecvt>
-#include <optional>
-#include <Windows.h>
-#include <TlHelp32.h>
-#include <psapi.h>
+#include <Windows.h> 
 
-#include "flutter/generated_plugin_registrant.h"
-#include <flutter/binary_messenger.h>
-#include <flutter/standard_method_codec.h>
-#include <flutter/method_channel.h>
-#include <flutter/method_result_functions.h>
-#include <ScreenCapture.h>
+// إضافة ملف كشف تسجيل الشاشة
+#include "ScreenCapture.h"
 
-using namespace std;
-bool IsScreenRecordingCurrentlyInProgress() {
-    // Define this function or include its header file if it is defined elsewhere.
-    return false;
-}
-FlutterWindow::FlutterWindow(const flutter::DartProject &project)
-        : project_(project) {}
+// متغير عالمي لمتابعة حالة النافذة 
+bool isRecording = false;
 
-FlutterWindow::~FlutterWindow() {}
+LRESULT CALLBACK FlutterWindow::MessageHandler(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) noexcept {
 
-void initMethodChannel(flutter::FlutterEngine *flutter_instance) {
-    const static std::string channel_name("test_channel");
-    auto channel = std::make_shared<flutter::MethodChannel<>>(
-            flutter_instance->messenger(), channel_name,
-            &flutter::StandardMethodCodec::GetInstance());
+  // تمكين فلتر من معالجة الرسائل
+  if (flutter_controller_) {
+    auto result = flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam, lparam);
+    if (result) return *result;
+  }
 
-    channel->SetMethodCallHandler(
-            [](const flutter::MethodCall<>& call,
-               std::unique_ptr<flutter::MethodResult<>> result) {
-                if (call.method_name().compare("test") == 0) {
-                    // Check for screen recording
-                    if (IsScreenRecordingCurrentlyInProgress()) {
-                        // Send message back to Dart
-                        result->Success("Screen recording detected!");
-                        return;
-                    }
-                    // Existing process listing code...
-                } else {
-                    result->NotImplemented();
-                }
-            });
-}
+  switch(message) {
 
-bool FlutterWindow::OnCreate() {
-    if (!Win32Window::OnCreate()) {
-        return false;
+    case WM_ACTIVATEAPP: {
+
+      // الكشف عن بدء/إنهاء تسجيل الشاشة
+      bool isCurrentlyRecording = IsScreenRecordingCurrentlyInProgress();  
+
+      // تبديل حالة مرئية النافذة
+      if(isCurrentlyRecording != isRecording) {
+        isRecording = isCurrentlyRecording;
+        ShowWindow(hwnd, isRecording ? SW_HIDE : SW_SHOW);
+      }
+
+      break;
     }
 
-    RECT frame = GetClientArea();
+    // معالجة رسائل أخرى
 
-    // The size here must match the window dimensions to avoid unnecessary surface
-    // creation / destruction in the startup path.
-    flutter_controller_ = std::make_unique<flutter::FlutterViewController>(
-            frame.right - frame.left, frame.bottom - frame.top, project_);
-    // Ensure that basic setup of the controller was successful.
-    if (!flutter_controller_->engine() || !flutter_controller_->view()) {
-        return false;
-    }
-    RegisterPlugins(flutter_controller_->engine());
-    // initialize method channel here ******
-    initMethodChannel(flutter_controller_->engine()); 
+  }
 
-    SetChildContent(flutter_controller_->view()->GetNativeWindow());
-    return true;
-}
+  return Win32Window::MessageHandler(hwnd, message, wparam, lparam);
 
-void FlutterWindow::OnDestroy() {
-    if (flutter_controller_) {
-        flutter_controller_ = nullptr;
-    }
-
-    Win32Window::OnDestroy();
-}
-
-LRESULT
-FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
-                              WPARAM const wparam,
-                              LPARAM const lparam)
-
-noexcept {
-// Give Flutter, including plugins, an opportunity to handle window messages.
-if (flutter_controller_) {
-std::optional <LRESULT> result =
-        flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
-                                                      lparam);
-if (result) {
-return *
-result;
-}
-}
-
-switch (message) {
-case WM_FONTCHANGE:
-flutter_controller_->engine()->ReloadSystemFonts();
-return 0;
-}
-
-return
-Win32Window::MessageHandler(hwnd, message, wparam, lparam
-);
 }
